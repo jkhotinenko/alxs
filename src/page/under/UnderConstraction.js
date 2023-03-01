1a3cf26c2d7d04dc90966f364deb818a5be2ad13
Yaroslav Khotinenko <jkhotinenko@gmail.com>
2023-03-01 22:02:56 +0200

    Underground

---
+++ src/page/under/UnderConstraction.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const UnderConstraction = () => {
+    return (
+        <div className="under">
+            <img className="imgc" src="medium.png"/>
+
+            <h1>
+                Сайт у розробці
+            </h1>
+            <h1>
+                The site is under development
+            </h1>
+        </div>
+
+    );
+};
+
+export {UnderConstraction};
